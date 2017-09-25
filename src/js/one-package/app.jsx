3ad5b7843ea3b045c0e031652ea877ba3bd74efa
Earl Debicki <bartosz.debicki@uxpin.com>
2017-09-25 18:01:08 +0200

    add one package basic less & js files

--- src/js/one-package/app.jsx
+++ src/js/one-package/app.jsx
@@ -1,10 +1,25 @@
 import React from 'react';
-import '../../less/views/one-package.less';
+
+import IconElement from '../components/iconElement';
+
+import { ONE_ICONS } from './iconsList.js';
+
+import '../../less/views/full-package.less';
 
 export default class App extends React.Component {
   render() {
     return (
-      console.log('one package')
+      <div className="reactWrapper">
+        <h5 className="title">fun with icon fonts</h5>
+        <h1 className="headline">one font file (general)</h1>
+        <ul className="iconsList iconsList--fullPackage">
+          {ONE_ICONS.map((item, index) => {
+            return(
+              <IconElement iconShape={item} key={index} />
+            )
+          })}
+        </ul>
+      </div>
     )
   }
 }
