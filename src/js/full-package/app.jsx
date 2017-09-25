7512c77643c2e4bc5ec0cd4d436d97b34ce893a4
Earl Debicki <bartosz.debicki@uxpin.com>
2017-09-25 14:49:07 +0200

    add title + layout builders

--- src/js/full-package/app.jsx
+++ src/js/full-package/app.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import '../../less/views/full-package.less';
 
-
 export default class App extends React.Component {
   render() {
     return (
-      console.log('full package')
+      <div className="reactWrapper">
+        <h5 className="title">fun with icon fonts</h5>
+        <h1 className="headline">icons compiled in one font file</h1>
+      </div>
     )
   }
 }
